746bd02465d6bd859b468cdc2de6a3bbb082d304
Dilson24 <dilacn97@hotmail.com>
2023-10-19 21:02:26 -0500

    [NEW]-add landin page

---
+++ index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Dilson Alexander Cruz Nivia">
+    <link rel="stylesheet" href="Css/styles.css" type="text/css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <script src=""></script>
+</head>
+
+<body>
+    <main class="container">
+        <div class="content">
+            <div class="contentImg">
+                <img src="Img/logo.svg" alt="img logo">
+                <img src="Img/header.png" alt="header">
+            </div>
+            <div class="contentLogin">
+                <h1 class="headlineOne">Conectando el mundo, un amigo a la vez</h1>
+                <div class="contentBtn">
+                    <h3 class="headlineTwo">Únete hoy</h3>
+                    <button class="signUp">Registrase</button>
+                    <button class="signIn">Iniciar Sesion</button>
+                </div>
+            </div>
+        </div>
+        <footer>
+            <div class="line"></div>
+            <div class="footerContact" id="contact">
+                <img class="imgFooter" src="Img/footerlogo.svg" alt="footer logo">
+                <div class="socialMedia">
+                    <div class="facebook">
+                        <a href="https://www.facebook.com/" target="_blank">
+                            <i class="fa-brands fa-facebook-f"></i>
+                        </a>
+                    </div>
+                    <div class="instagram">
+                        <a href="https://www.instagram.com/dilson.cruz/" target="_blank">
+                            <i class="fab fa-instagram"></i>
+                        </a>
+                    </div>
+                    <div class="github">
+                        <a href="https://github.com/Dilson24" target="_blank">
+                            <i class="fa-brands fa-github"></i>
+                        </a>
+                    </div>
+                    <div class="linkedin">
+                        <a href="https://www.linkedin.com/in/dilson-alexander-cruz-nivia-adsi/" target="_blank">
+                            <i class="fa-brands fa-linkedin-in"></i>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="contact-us">
+                <h3 class="brad">
+                    &copy; 2023 Dilson Alexander Cruz Nivia | Todos los
+                    derechos reservados.
+                </h3>
+            </div>
+            </div>
+        </footer>
+    </main>
+</body>
+
+</html>
